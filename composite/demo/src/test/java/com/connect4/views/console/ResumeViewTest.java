0a1650aaf79875fe65ee2ffc9c4d4b9175292a30
jromero789 <juanromerogonzalez9@gmail.com>
2022-11-14 06:37:11 +0100

    Add resume view test

---
+++ composite/demo/src/test/java/com/connect4/views/console/ResumeViewTest.java
@@ -0,0 +1,54 @@
+package com.connect4.views.console;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.MockedStatic;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.connect4.controllers.ResumeController;
+import com.connect4.models.Session;
+import com.utils.views.Console;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class ResumeViewTest {
+
+    @Mock
+    private Console console;
+
+    private ResumeController resumeController;
+
+    private ResumeView resumeView;
+
+    @BeforeEach
+    public void beforeEach(){
+        this.resumeController = new ResumeController(new Session());
+        this.resumeView = new ResumeView();
+    }
+
+    @Test
+    public void testGivenNewGameIsFalseWhenInteractThenIsFalse() {
+        try (MockedStatic<Console> console = mockStatic(Console.class)) {
+            console.when(Console::getInstance).thenReturn(this.console);
+            when(this.console.readString(anyString())).thenReturn("n");
+            assertThat(this.resumeView.interact(this.resumeController), is(false));
+        }
+    }
+
+    @Test
+    public void testGivenNewGameIsTrueWhenInteractThenIsTrue() {
+        try (MockedStatic<Console> console = mockStatic(Console.class)) {
+            console.when(Console::getInstance).thenReturn(this.console);
+            when(this.console.readString(anyString())).thenReturn("y");
+            assertThat(this.resumeView.interact(this.resumeController), is(true));
+        }
+    }
+
+}
