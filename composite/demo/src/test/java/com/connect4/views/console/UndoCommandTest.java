de26e2d6afe291ad594ae8ecea2762429ffc7077
jromero789 <juanromerogonzalez9@gmail.com>
2022-11-14 06:48:03 +0100

    Add undoCommandTest

---
+++ composite/demo/src/test/java/com/connect4/views/console/UndoCommandTest.java
@@ -0,0 +1,45 @@
+package com.connect4.views.console;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.connect4.controllers.PlayController;
+import com.connect4.types.Color;
+import com.connect4.views.Message;
+import com.utils.models.CommandTest;
+
+@ExtendWith(MockitoExtension.class)
+public class UndoCommandTest extends CommandTest {
+
+    @Mock
+    private PlayController playController;
+
+    @Override
+    public Command getCommand() {
+        return new UndoCommand(playController);
+    }
+
+    @Override
+    public boolean isActive() {
+        return this.getCommand().isActive();
+    }
+
+    @Override
+    public String getTitle() {
+        return Message.UNDO_COMMAND.toString();
+    }
+
+    @Test
+    public void testGivenUndoCommandWhenExecuteThenAdd() {
+        when(this.playController.getColor(any())).thenReturn(Color.RED);
+        this.getCommand().execute();
+        verify(this.playController).undo();
+    }
+    
+}
