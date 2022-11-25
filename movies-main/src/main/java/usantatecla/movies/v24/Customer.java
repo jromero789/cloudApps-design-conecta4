a208bace9cbc65eff16e035e1ff12183aca816eb
jromero789 <juanromerogonzalez9@gmail.com>
2022-11-25 03:08:55 +0100

    create v24

---
+++ movies-main/src/main/java/usantatecla/movies/v24/Customer.java
@@ -0,0 +1,51 @@
+package usantatecla.movies.v24;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Customer {
+
+	private String name;
+	
+	private List<Rental> rentals;
+
+	public Customer(String name) {
+		this.name = name;
+		rentals = new ArrayList<Rental>();
+	}
+
+	public void addRental(Rental rental) {
+		rentals.add(rental);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String statement() {
+		String result = "Rental Record for " + this.getName() + "\n";
+		for (Rental rental : rentals) {
+			result += "\t" + rental.getTitle() + "\t" + String.valueOf(rental.getCharge()) + "\n";
+		}
+		result += "Amount owed is " + String.valueOf(this.getTotalCharge()) + "\n";
+		result += "You earned " + String.valueOf(this.getTotalFrequentRenterPoints()) + " frequent renter points";
+		return result;
+	}
+
+	private double getTotalCharge() {
+		double result = 0;
+		for (Rental rental : rentals) {
+			result += rental.getCharge();
+		}
+		return result;
+	}
+	
+	private int getTotalFrequentRenterPoints() {
+		int result = 0;
+		for (Rental rental : rentals) {
+			result += rental.getFrequentRenterPoints();
+		}
+		return result;
+	}
+
+}
